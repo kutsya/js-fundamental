09ede4bd209c6ff8a517605b6793f0d83d73e08c
kutsya <morzik754@gmail.com>
2023-03-09 18:08:36 +0200

    Added info js

--- INTRODUCTION TO JAVASCRIPT/main.js
+++ INTRODUCTION TO JAVASCRIPT/main.js
@@ -2,7 +2,7 @@
 
 console.log('Morozova');
 
-//! Variables (Task 3)
+//? Task 3 (Variables)
 
 let door = 's';
 
@@ -14,3 +14,28 @@ console.log('tableSize', tableSize);
 
 //? Task 4
 
+let number = 15; //*Number
+console.log(number);
+let string = 'home'; //*String
+console.log(string);
+let boolean = true; //*Boolean
+console.log(boolean);
+let a = null; //*Null
+console.log(a);
+let b; //*Undefined 
+console.log(b);
+
+//? Task 5
+
+const isAdult = confirm('Are you of legal age?');
+
+//? Task 7
+
+let login = prompt('Dear User, please write your login');
+alert(login);
+let email = prompt('Dear User, please write your email', 'gmail.com');
+alert(email);
+let password = prompt('Dear user, please write ypor password', 'qwerty');
+alert(password);
+
+//? Task 8
