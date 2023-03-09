70f4768324b572dde8b0a1ecd4ab2c4d29edca1f
kutsya <121502899+kutsya@users.noreply.github.com>
2023-03-09 13:15:08 +0000

    Added html and js

---
+++ INTRODUCTION TO JAVASCRIPT/main.js
@@ -0,0 +1,16 @@
+//? Task 2
+
+console.log('Morozova');
+
+//! Variables (Task 3)
+
+let door = 's';
+
+let table = 'xl';
+let tableSize = table; // Copy by value
+
+console.log('door', door);
+console.log('tableSize', tableSize);
+
+//? Task 4
+
