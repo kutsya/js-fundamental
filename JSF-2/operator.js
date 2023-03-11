55eb8324b80c660fa0a8f747c4cc0b27063b6256
kutsya <morzik754@gmail.com>
2023-03-11 18:57:41 +0200

    Added js file

---
+++ JSF-2/operator.js
@@ -0,0 +1,24 @@
+//? Task 1
+
+console.log('a < b < c', 'a' < 'b' < 'c');
+
+//? Task 2
+
+let x = 1;
+let y = 2;
+
+let res1 = (xy); // Допишіть код, необхідно використовувати змінні x і y
+console.log(res1); // ""12""
+console.log(typeof res1); // ""string""
+
+let res2 = // Допишіть код, необхідно використовувати змінні x і y
+console.log(res2); // ""true2""
+console.log(typeof res2); // ""string""
+
+let res3 = // Допишіть код, необхідно використовувати змінні x і y
+console.log(res3); // true
+console.log(typeof res3); // ""boolean""
+
+let res4 = // Допишіть код, необхідно використовувати змінні x і y
+console.log(res4); // NaN
+console.log(typeof res4); // ""number""
