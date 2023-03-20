df6c787cd97770da67b2605e5cd1e5d85c7102fe
kutsya <morzik754@gmail.com>
2023-03-20 13:31:57 +0200

    Added 1, 3, 5, task

--- JSF-4/main.js
+++ JSF-4/main.js
@@ -1,14 +1,42 @@
-//? Task 1
+//? Task 1 
 
+function sumSliceArray(arr, first, second) {
+    //! Перевірка, чи є передані аргументи числами
+    if (typeof first !== "number" || typeof second !== "number") {
+    throw new Error("Аргументи повинні бути числами");
+    }
 
+    //! Перевірка, чи не виходять передані індекси за межі масиву
+    if (first >= arr.length || second >= arr.length) {
+    throw new Error("Індекс виходить за межі масиву");
+    }
 
-//? Task 2
+    return arr[first] + arr[second];
+};
+
+//? Task 2 
 
 
 
 //? Task 3
 
+function calcRectangleArea(width, height) {
+    if (typeof width !== 'number' || typeof height !== 'number') {
+    throw new Error('Передані параметри повинні бути числовими');
+    }
 
+    return width * height;
+};
+
+// Приклад виклику функції та обробки винятку
+try {
+    const width = 'не число';
+    const height = 10;
+    const area = calcRectangleArea(width, height);
+    console.log(`Площа прямокутника: ${area}`);
+} catch (error) {
+    console.error(`Сталася помилка: ${error.message}`);
+};
 
 //? Task 4
 
@@ -16,6 +44,32 @@
 
 //? Task 5
 
+class UserException extends Error {
+    constructor(message) {
+    super(message);
+    this.name = 'UserException';
+    }
+};
 
+function showUser(id) {
+    if (id < 0) {
+    throw new UserException('ID must not be negative: ' + id);
+    }
+    return { id };
+};
 
-//? Task 6
+function showUsers(ids) {
+    const result = [];
+    for (const id of ids) {
+    try {
+        const user = showUser(id);
+        result.push(user);
+    } catch (e) {
+        console.error(e.message);
+    }
+    }
+    return result;
+};
+
+console.log(showUsers([7, -12, 44, 22]));
+
