b8a6e00cfa140fed4c9b9871f92f9378a47d4c2b
kutsya <morzik754@gmail.com>
2023-04-02 14:51:32 +0300

    Added task 1,2 and 3 jsf-7

---
+++ JSF-7/main.js
@@ -0,0 +1,22 @@
+//? Task 1
+
+window.open('', '', 'width=300, height=300');
+
+setTimeout(function() {
+    window.resizeTo(500, 500);
+}, 2000);
+
+setTimeout(function() {
+    window.moveTo(200, 200);
+}, 4000);
+
+setTimeout(function() {
+    window.close();
+}, 6000);
+
+//? Task 2
+
+// html
+
+//? Task 3
+
